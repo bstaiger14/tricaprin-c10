83d7bb277f117e433d740f160d53ac2102a42c84
bstaiger14 <144149645+bstaiger14@users.noreply.github.com>
2026-06-08 13:26:58 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,12 +53,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const hotspots = document.querySelectorAll('.hotspot');
   const cards = document.querySelectorAll('.hotspot-card');
   const closeBtns = document.querySelectorAll('.close-card');
-  const body = document.body;
 
   function closeAllCards() {
     cards.forEach(card => card.classList.remove('active'));
     hotspots.forEach(btn => btn.classList.remove('active'));
-    body.classList.remove('popup-active');
   }
 
   // Open Tooltip when + is clicked
@@ -77,7 +75,6 @@ document.addEventListener("DOMContentLoaded", () => {
         btn.classList.add('active');
         if(targetCard) {
           targetCard.classList.add('active');
-          body.classList.add('popup-active'); // Dims background
         }
       }
     });
@@ -91,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  // Close tooltips if the user clicks anywhere else on the screen (the dimmed background)
+  // Close tooltips if the user clicks anywhere else on the screen
   document.addEventListener('click', (e) => {
     if (!e.target.closest('.hotspot-card') && !e.target.closest('.hotspot')) {
       closeAllCards();
